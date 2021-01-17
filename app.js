58978efafb4f766bdbe2743b9f7cc839a25a0205
Rchi Lugtu <lugturchi@gmail.com>
2021-01-18 12:29:03 +1300

    Now pointing to the right db

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/reviews');
 
-// const dbURL = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';  // production || development
-const dbURL = 'mongodb://localhost:27017/yelp-camp';  // production || development
+const dbURL = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';  // production || development
+// const dbURL = 'mongodb://localhost:27017/yelp-camp';  // production || development
 
 // Connect to the yelp-camp mongo database
 mongoose.connect(dbURL, {
